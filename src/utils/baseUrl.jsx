2b57517b2c54540fc3075900e35aac37007dfb8b
Aime_Bruce <98209400+bruceimmort@users.noreply.github.com>
2024-06-22 03:32:37 +0200

    fix: base url

--- src/utils/baseUrl.jsx
+++ src/utils/baseUrl.jsx
@@ -1,10 +1,10 @@
-let stage = "dev";
+let stage = "prod";
 let baseUrl = "";
 
 if (stage == "dev"){
     baseUrl = "http://localhost:3000/admin/";
 }else if (stage == "prod"){
-    // set baseUrl to the Url of deployed backend
+    baseUrl = "https://booking-sys-servers.onrender.com/admin/"
 }
 
 export default baseUrl;
