ea7d7231f973f44d2e4abf4d398c93fd77faca75
Aime_Bruce <98209400+bruceimmort@users.noreply.github.com>
2024-06-30 01:12:48 +0200

    Express login integration

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,35 @@ import Adminlogin from "./Pages/Admin/Index";
 import Notification from "./components/pages/Notification";
 import { verifyToken } from "./utils/apiFunctions";
 import DesktopOnly from "./Pages/Other/DesktopOnly";
+import ExpressLogin from "./Pages/Express/Login";
 
 export default function App() {
   const [adminLogedIn, setAdminLogedIn] = useState(false);
-  const admin_token = localStorage.getItem("access_token");
+  const [expressLogedIn, setExpressLogedIn] = useState(false);
+  const accessToken = localStorage.getItem("access_token");
+  const expressAccess = localStorage.getItem("express_access");
 
   useEffect(() => {
     // Verify the token (if there is one) and set login state
-    if(admin_token){
+    if(accessToken){
       if(verifyToken()){
         setAdminLogedIn(true);
       } else {
         setAdminLogedIn(false);
       }
     }
+    if(expressAccess){
+      setExpressLogedIn(true);
+    } else {
+      setExpressLogedIn(false);
+    }
   }, []);
   return (
     <Routes>
-      <Route path="//" element={<Adminlogin/>}></Route>
-      <Route path="./admin" element={<Adminlogin/>}></Route>
-      <Route path="./sandbox" element={<DesktopOnly/>}></Route>
+      <Route path="/" element={<Adminlogin login={setExpressLogedIn}/>}></Route>
+      <Route path="/admin" element={adminLogedIn ? <AdminDashboard login={setExpressLogedIn}/> : <Adminlogin login={setExpressLogedIn}/>}></Route>
+      <Route path="/express" element={expressLogedIn ? <ExpressDashboard login={setExpressLogedIn}/> : <ExpressLogin login={setExpressLogedIn}/>}></Route>
+      <Route path="/sandbox" element={<DesktopOnly/>}></Route>
     </Routes>
   )
 }
--- src/utils/baseUrl.jsx
+++ src/utils/baseUrl.jsx
@@ -1,10 +1,13 @@
-let stage = "prod";
+let stage = "dev";
 let baseUrl = "";
+let expressBaseURL = ""
 
 if (stage == "dev"){
     baseUrl = "http://localhost:3000/admin/";
+    expressBaseURL = "http://localhost:3000/express/"
 }else if (stage == "prod"){
     baseUrl = "https://booking-sys-servers.onrender.com/admin/"
+    expressBaseURL = "https://booking-sys-servers.onrender.com/express/"
 }
 
-export default baseUrl;
+export default {baseUrl, expressBaseURL};
