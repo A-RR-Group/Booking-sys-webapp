60c2d712b09f11d22d012c0c3b5f59548be7b91f
Aime_Bruce <98209400+bruceimmort@users.noreply.github.com>
2024-06-29 09:15:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3030
+    host: '0.0.0.0',
+    port: 3000,
   },
-  preview: {
-    port: 8080
-  }
 })
